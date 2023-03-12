df6d423bdab68663ef5c703cebce9580dd206f26
retardH <loowmaad@gmail.com>
2023-03-12 12:49:43 +0630

    commit for assignment 6

---
+++ bank_project/bank.c
@@ -0,0 +1,18 @@
+//
+// Created by HtetZarni on 20/02/2023.
+//
+
+#include "stdio.h"
+#include "zoom_online_bank.h"
+
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+
+//    integer_to_char(1000);
+
+    return 0;
+}
